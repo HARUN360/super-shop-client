f96b79fc22599a51e228f78ab272d781ab21c9d5
HARUN360 <mdharunorroshid301204@gmail.com>
2024-08-18 15:06:01 +0600

    firebase added

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -6,15 +6,15 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+console.log('firebase', import.meta.env.VITE_IMGBB_API_KEY2);
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAyVADwwB4gILOtC5Lb9JqfJcQl96rAjW4",
-  authDomain: "super-shop-6acda.firebaseapp.com",
-  projectId: "super-shop-6acda",
-  storageBucket: "super-shop-6acda.appspot.com",
-  messagingSenderId: "439497643770",
-  appId: "1:439497643770:web:b447f2ac01bb446c721b43"
+  apiKey:import.meta.env.VITE_IMGBB_API_KEY1,
+  authDomain:import.meta.env.VITE_IMGBB_API_KEY2,
+  projectId:import.meta.env.VITE_IMGBB_API_KEY3,
+  storageBucket:import.meta.env.VITE_IMGBB_API_KEY4,
+  messagingSenderId:import.meta.env.VITE_IMGBB_API_KEY5,
+  appId:import.meta.env.VITE_IMGBB_API_KEY6,
 };
 
 // Initialize Firebase
